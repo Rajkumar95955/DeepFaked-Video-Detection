418e1f4256cc7b2a2b98ce25c629b1482a0bfc1b
Sanyog Dani <sanyogdani1967@gmail.com>
2024-12-30 23:12:56 +0530

    new

---
+++ Django Application/bin/gunicorn_start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NAME="project_settings"                                 
+DJANGODIR=/app             
+SOCKFILE=/app/run/gunicorn.sock  
+NUM_WORKERS=3                                    
+DJANGO_SETTINGS_MODULE=project_settings.settings             
+DJANGO_WSGI_MODULE=project_settings.wsgi                     
+
+echo "Starting $NAME as `whoami`"
+
+RUNDIR=$(dirname $SOCKFILE)
+test -d $RUNDIR || mkdir -p $RUNDIR
+
+ gunicorn project_settings.wsgi:application --bind=unix:$SOCKFILE --workers $NUM_WORKERS --timeout 600
